b2b38518912c910f2ca8d43f0d8f15235458a684
Mike Doerfler <mike.doerfler@gmail.com>
2012-09-29 16:50:28 -0500

    starting on chapter 1 - and learning f# at same time :)

---
+++ ThinkStats/ThinkStats.Console/Pregnancy.fs
@@ -0,0 +1,31 @@
+﻿module Pregnancies
+
+open System
+
+type Pregnancy = {
+    CaseId : int;
+    PrgLength : int;
+    Outcome : int;
+    BirthOrder: int option;
+    FinalWeight : float
+}
+
+let isLiveBirth preg = 
+    match preg.BirthOrder with
+    | Some x -> true
+    | None -> false
+
+let parseNullableInt text =
+    match Int32.TryParse text with
+    | (true, result) -> Some result
+    | (false, _) -> None
+
+let parsePregnancy (line:string) =
+    { 
+        CaseId = Int32.Parse(line.Substring(0, 12));
+        PrgLength = Int32.Parse(line.Substring(274, 2));
+        Outcome = Int32.Parse(line.Substring(276, 1));
+        BirthOrder = parseNullableInt(line.Substring(277, 2));
+        FinalWeight = Double.Parse(line.Substring(422, 17));
+    }
+
---
+++ ThinkStats/ThinkStats.Console/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.IO
+
+let ex1_3 = 
+    let text = File.ReadAllLines("2002FemPreg.dat")
+    let content = Array.map Pregnancies.parsePregnancy text 
+
+    // #1
+    printfn "%d pregnancies" content.Length
+
+    // #2
+    let liveBirthCount = 
+        content
+        |> Array.filter Pregnancies.isLiveBirth 
+        |> Array.length
+
+    printfn "%d live births " liveBirthCount
+
+
+    0
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    // return an integer exit code
+    ex1_3
