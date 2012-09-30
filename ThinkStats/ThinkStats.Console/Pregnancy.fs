885a9588041c10cebb2cd754386b009c80e1e227
Mike Doerfler <mike.doerfler@gmail.com>
2012-09-30 17:39:35 -0500

    coded exercise 1-3

--- ThinkStats/ThinkStats.Console/Pregnancy.fs
+++ ThinkStats/ThinkStats.Console/Pregnancy.fs
@@ -3,10 +3,14 @@
 open System
 
 type Pregnancy = {
-    CaseId : int;
+    CaseId : string;
+    BabySex : string;
+    BirthWeightPounds: string;
+    BirthWeightOunces : string;
     PrgLength : int;
-    Outcome : int;
+    Outcome : string;
     BirthOrder: int option;
+    AgePregnancy : string;
     FinalWeight : float
 }
 
@@ -20,12 +24,28 @@ let parseNullableInt text =
     | (true, result) -> Some result
     | (false, _) -> None
 
+    (*
+            ('caseid', 1, 12, int),
+            ('nbrnaliv', 22, 22, int),
+            ('babysex', 56, 56, int),
+            ('birthwgt_lb', 57, 58, int),
+            ('birthwgt_oz', 59, 60, int),
+            ('prglength', 275, 276, int),
+            ('outcome', 277, 277, int),
+            ('birthord', 278, 279, int),
+            ('agepreg', 284, 287, int),
+            ('finalwgt', 423, 440, float),
+    *)
 let parsePregnancy (line:string) =
     { 
-        CaseId = Int32.Parse(line.Substring(0, 12));
+        CaseId = line.Substring(0, 12);
+        BabySex = line.Substring(55,1);
+        BirthWeightPounds = line.Substring(57,2);
+        BirthWeightOunces = line.Substring(58,2);
         PrgLength = Int32.Parse(line.Substring(274, 2));
-        Outcome = Int32.Parse(line.Substring(276, 1));
+        Outcome = line.Substring(276, 1);
         BirthOrder = parseNullableInt(line.Substring(277, 2));
+        AgePregnancy = line.Substring(283, 3);
         FinalWeight = Double.Parse(line.Substring(422, 17));
     }
 
--- ThinkStats/ThinkStats.Console/Program.fs
+++ ThinkStats/ThinkStats.Console/Program.fs
@@ -2,25 +2,59 @@
 open System.IO
 
 let ex1_3 = 
-    let text = File.ReadAllLines("2002FemPreg.dat")
-    let content = Array.map Pregnancies.parsePregnancy text 
+    let content = 
+        File.ReadLines("2002FemPreg.dat")
+        |> Seq.map Pregnancies.parsePregnancy
+        |> Seq.toArray
 
     // #1
     printfn "%d pregnancies" content.Length
 
     // #2
-    let liveBirthCount = 
+    let liveBirths = 
         content
         |> Array.filter Pregnancies.isLiveBirth 
+
+    let liveBirthCount = 
+        liveBirths
         |> Array.length
 
     printfn "%d live births " liveBirthCount
 
+    // #3
+    let firstBorn = 
+        liveBirths
+        |> Array.filter (fun (x) -> x.BirthOrder.Value = 1)
 
-    0
+    let notFirstBorn = 
+        liveBirths
+        |> Array.filter (fun (x) -> x.BirthOrder.Value <> 1)
+
+    printfn "number of live births for the first born: %d" firstBorn.Length
+    printfn "number of live births for the other born: %d" notFirstBorn.Length
+
+    // #4
+    let firstBornWeeksAvg = 
+        firstBorn
+        |> Array.averageBy (fun (x) -> (float)x.PrgLength)
+
+    let notFirstBornWeeksAvg = 
+        notFirstBorn
+        |> Array.averageBy (fun (x) -> (float)x.PrgLength)
+
+    printfn "average weeks of pregancy for the first born: %f" firstBornWeeksAvg
+    printfn "average weeks of pregancy for the not first born: %f" notFirstBornWeeksAvg
+
+    let diffInDays = (firstBornWeeksAvg - notFirstBornWeeksAvg) * 7.0
+    let diffInHours = (diffInDays) * 24.0
+
+    printfn "diff in days: %f" diffInDays
+    printfn "diff in hours: %f" diffInHours
+
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
     // return an integer exit code
     ex1_3
+    0
